781e4b8361d7705c469c2b644e0167bf5f4e6bfe
SamiVijarra <samivijarra1@gmail.com>
2026-03-31 20:51:49 -0300

    feat: add custom middlewares

--- routes/auth.js
+++ routes/auth.js
@@ -3,16 +3,19 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { crearUsuario, loginUsuario, revalidarToken} = require('../controllers/auth');
+
 const router = Router();
 
-const { crearUsuario, loginUsuario, revalidarToken} = require('../controllers/auth');
 
 router.post(
     '/new',
     [
         check('name', 'El nombre es obligatorio').not().isEmpty(),
         check('email', 'El email es obligatorio').isEmail(),
-        check('password', 'El password debe ser de al menos 6 caracteres').isLength({ min: 6 })
+        check('password', 'El password debe ser de al menos 6 caracteres').isLength({ min: 6 }),
+        validarCampos
     ],
     crearUsuario);
 
@@ -20,7 +23,8 @@ router.post(
     '/',
     [
         check('email', 'El email es obligatorio').isEmail(),
-        check('password', 'El password es obligatorio').not().isEmpty()
+        check('password', 'El password es obligatorio').not().isEmpty(),
+        validarCampos
     ],
     loginUsuario);
 
