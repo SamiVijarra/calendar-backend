681291f081e286f0adc57ec870d2ee6fc36268bc
SamiVijarra <samivijarra1@gmail.com>
2026-04-01 23:02:25 -0300

    feat: create users in the db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,25 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario');
 
+const crearUsuario = async ( req, res = response ) => {
+//    const { name, email, password } = req.body;
 
-const crearUsuario = ( req, res = response ) => {
-    const { name, email, password } = req.body;
+    try {
+        const usuario = new Usuario( req.body );
 
-    res.status(201).json({
-        ok: true,
-        msg: 'register',
-        name,
-        email,
-        password
-    })
+        await usuario.save();
+
+        res.status(201).json({
+            ok: true,
+            msg: 'register',
+            usuario
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Error creating user'
+        });
+    } 
 };
 
 const loginUsuario = ( req, res = response ) => {
