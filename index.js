eb71091f997227d9386fa9a8bdfe45b646900c21
SamiVijarra <samivijarra1@gmail.com>
2026-04-03 00:06:40 -0300

    feat: add configure cors

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 require('dotenv').config();
 
+const cors = require('cors'); 
+
 const { dbConnection } = require('./database/config');
 
 const app = express();
 
 dbConnection();
+
+app.use(cors());
 
 app.use( express.static('public') );
 
