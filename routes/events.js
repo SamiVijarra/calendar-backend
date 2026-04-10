c0ca919d1b5b6a833c3810d84cf5801803127a85
Sami <samivijarra1@gmail.com>
2026-04-10 16:15:03 -0300

    feat: add validate fields

--- routes/events.js
+++ routes/events.js
@@ -1,5 +1,9 @@
 const { Router } = require('express');
 const { validarJWT } = require('../middlewares/validar-jwt');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { isDate } = require('../helpers/isDate');
+
 
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
 
@@ -8,7 +12,15 @@ router.use(validarJWT);
 
 router.get('/', getEventos);
 
-router.post('/', crearEvento); 
+router.post(
+  '/',
+  [
+    check('title', 'El título es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'Fecha de fin es obligatoria').custom(isDate),
+    validarCampos,
+  ],
+  crearEvento); 
 
 router.put('/:id', actualizarEvento); 
 
