ba41023cf401ddddd96eb3e1423f00c80ff684f7
Sami <samivijarra1@gmail.com>
2026-04-17 00:17:47 -0300

    fix: correct routes auth

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use( express.json() );
 app.use('/api/auth', require('./routes/auth') );
 app.use('/api/events', require('./routes/events') );
 
-app.use('*', (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
