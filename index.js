2221a883942c4939ff05a43f0dc7b42194c8cb23
Sami <samivijarra1@gmail.com>
2026-04-17 00:01:46 -0300

    fix: correct routes auth

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 require('dotenv').config();
@@ -18,6 +20,10 @@ app.use( express.json() );
 
 app.use('/api/auth', require('./routes/auth') );
 app.use('/api/events', require('./routes/events') );
+
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en el puerto ${ process.env.PORT }`);
